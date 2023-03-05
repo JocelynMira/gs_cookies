92da737c0b78d432ab4fc60db167c771d02c821a
JocelynMira <mkoshiba02@gmail.com>
2023-03-04 15:40:00 -1000

    initial commit (no validations yet)

---
+++ flask_app/controllers/cookie_orders_controller.py
@@ -0,0 +1,55 @@
+from flask_app import app
+from flask import render_template, redirect, request
+from flask_app.models.cookie_order_model import Order
+
+
+# route to dashboard displaying orders
+@app.route('/')
+def index():
+    return redirect('/cookies')
+
+# READ 
+@app.route('/cookies')
+def get_all_orders():
+    orders = Order.get_all()
+    return render_template ('index.html', all_orders = orders)
+
+# route to create new order
+@app.route('/place_order')
+def place_order():
+    return render_template ('new_order.html')
+
+# CREATE
+@app.route('/new_order', methods = ['POST'])
+def new_order():
+    data = { 
+        'name' : request.form['name'],
+        'cookie_type' : request.form['cookie_type'],
+        'number_of_boxes' : request.form['number_of_boxes']
+    }
+    Order.save(data)
+    return redirect ('/')
+
+# route to change order
+@app.route('/change/<int:id>')
+def change_one_order(id):
+    order = Order.one_order(id)
+    return render_template ('change_order.html', order = order )
+
+# UPDATE
+@app.route('/change_order/<int:id>', methods = ['POST'])
+def change(id):
+    data = {
+        'id': id,
+        'name': request.form['name'],
+        'cookie_type': request.form['cookie_type'],
+        'number_of_boxes': request.form['number_of_boxes']
+    }
+    Order.change(data)
+    return redirect ('/')
+
+# DELETE
+@app.route('/delete_order/<int:id>')
+def delete(id):
+    Order.delete(id)
+    return redirect ('/')
---
+++ flask_app/models/cookie_order_model.py
@@ -0,0 +1,50 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash
+
+class Order:
+    db = "gs_cookies"
+
+    def __init__(self, data):
+        self.id = data['id']
+        self.name = data['name']
+        self.cookie_type = data['cookie_type']
+        self.number_of_boxes = data['number_of_boxes']
+
+    # CREATE
+    @classmethod
+    def save(cls, data):
+        query ="INSERT INTO cookie_orders (name, cookie_type, number_of_boxes) VALUES ( %(name)s, %(cookie_type)s, %(number_of_boxes)s);"
+        return connectToMySQL(cls.db).query_db(query, data)
+
+    # READ (all)
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM cookie_orders;"
+        results = connectToMySQL(cls.db).query_db(query)
+        orders = [] 
+
+        for order in results:
+            orders.append( cls(order) )
+        return orders
+    
+    # READ (one)
+    @classmethod
+    def one_order(cls, id):
+        query = "SELECT * FROM cookie_orders WHERE id = %(id)s"
+        results = connectToMySQL(cls.db).query_db(query, {'id': id})
+        return cls(results[0])
+
+    # UPDATE 
+    @classmethod
+    def change(cls, data):
+        query = "UPDATE cookie_orders SET name = %(name)s, cookie_type = %(cookie_type)s, number_of_boxes = %(number_of_boxes)s WHERE id = %(id)s;"
+        results = connectToMySQL(cls.db).query_db(query, data)
+        return results 
+
+    # DELETE
+    @classmethod
+    def delete(cls, id):
+        query = "DELETE FROM cookie_orders WHERE id = %(id)s"
+        results = connectToMySQL(cls.db).query_db(query, {'id': id})
+        return results
+    
